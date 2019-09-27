ed318e31e505214e47ef0617a80cc9f98a7b03b1
itachi <qiji94@163.com>
2019-09-27 17:54:50 +0800

    uber日志库的使用

--- logdef.go
+++ logdef.go
@@ -8,26 +8,30 @@ import (
 	"time"
 )
 
-var logger *zap.Logger
+var Logger *zap.Logger
+
+type LogInfo struct {
+	LogPath  string `json:"log_path" toml:"log_path"`
+	LogLevel string `json:"log_level" toml:"log_level"`
+	LogAddr  string `json:"log_addr" toml:"log_addr"`
+}
 
 func HumanFormat(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
 }
-
-func logLevelListen(handler func(http.ResponseWriter, *http.Request),addr string){
+func logLevelListen(handler func(http.ResponseWriter, *http.Request), addr string) {
 	logLevelHandle := http.NewServeMux()
-	logLevelHandle.HandleFunc("/handle/loglevel",handler)
-	err := http.ListenAndServe(addr,logLevelHandle)
-	if err != nil{
+	logLevelHandle.HandleFunc("/handle/loglevel", handler)
+	err := http.ListenAndServe(addr, logLevelHandle)
+	if err != nil {
 		panic(err)
 	}
 }
-func Init(logpath string ,loglevel string ,addr string) * zap.Logger {
-	hook := lumberjack.Logger{Filename:logpath,MaxSize:500,MaxBackups:5,MaxAge:3,LocalTime:true,Compress:false}
+func Init(loginf *LogInfo) *zap.Logger {
+	hook := lumberjack.Logger{Filename: loginf.LogPath, MaxSize: 500, MaxBackups: 5, MaxAge: 3, LocalTime: true, Compress: false}
 	w := zapcore.AddSync(&hook)
 	var level zap.AtomicLevel
-
-	switch loglevel {
+	switch loginf.LogLevel {
 	case "debug":
 		level = zap.NewAtomicLevelAt(zap.DebugLevel)
 	case "warn":
@@ -37,56 +41,10 @@ func Init(logpath string ,loglevel string ,addr string) * zap.Logger {
 	default:
 		level = zap.NewAtomicLevel()
 	}
-
-	go logLevelListen(level.ServeHTTP,addr)
+	go logLevelListen(level.ServeHTTP, loginf.LogAddr)
 	encoder := zap.NewProductionEncoderConfig()
 	encoder.EncodeTime = HumanFormat
-	core := zapcore.NewCore(zapcore.NewConsoleEncoder(encoder),w,level)
-	logger := zap.New(core,zap.AddCaller())
-	return logger
-}
-
-func LogDebugS(key string,value string,desc ...string){
-	if len(desc) > 0{
-		logger.Debug(desc[0],zap.String(key,value))
-	}else{
-		logger.Debug("",zap.String(key,value))
-	}
-}
-func LogDebugI(key string,value int64, desc ...string ){
-	if len(desc) > 0{
-		logger.Debug(desc[0],zap.Int64(key,value))
-	}else{
-		logger.Debug("",zap.Int64(key,value))
-	}
-}
-func LogErrorS(key string,value string,desc ...string){
-	if len(desc) > 0{
-		logger.Error(desc[0],zap.String(key,value))
-	}else{
-		logger.Error("",zap.String(key,value))
-	}
-}
-func LogErrorI(key string,value int64, desc ...string ){
-	if len(desc) > 0{
-		logger.Error(desc[0],zap.Int64(key,value))
-	}else{
-		logger.Error("",zap.Int64(key,value))
-	}
-}
-
-func LogInfoS(key string,value string,desc ...string){
-	if len(desc) > 0{
-		logger.Info(desc[0],zap.String(key,value))
-	}else{
-		logger.Info("",zap.String(key,value))
-	}
-}
-
-func LogInfoI(key string,value int64, desc ...string ){
-	if len(desc) > 0{
-		logger.Info(desc[0],zap.Int64(key,value))
-	}else{
-		logger.Info("",zap.Int64(key,value))
-	}
+	core := zapcore.NewCore(zapcore.NewConsoleEncoder(encoder), w, level)
+	Logger = zap.New(core, zap.AddCaller())
+	return Logger
 }
